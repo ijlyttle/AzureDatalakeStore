28d0efad6ba2529044f992616a3aeb6f17e86faf
Ian Lyttle <ian.lyttle@schneider-electric.com>
2017-04-30 11:46:10 -0500

    try again at Travis

--- tests/testthat/test-adls.R
+++ tests/testthat/test-adls.R
@@ -2,14 +2,12 @@ context("adls")
 
 library("httr")
 
-adls_url_test <- adls_url(helper$account_test_name)
-
 test_that("can create url", {
 
   url_sample <-
-    "https://latte0test0do0not0use.azuredatalakestore.net/webhdfs/v1"
+    "https://foo.azuredatalakestore.net/webhdfs/v1"
 
-  expect_identical(adls_url_test, parse_url(url_sample))
+  expect_identical(adls_url("foo"), parse_url(url_sample))
 })
 
 test_that("adls constructor gives error when needed", {
@@ -21,7 +19,7 @@ test_that("adls constructor works", {
   skip_on_travis()
   skip_if_not(interactive())
 
-  adls_test <- adls(adls_url_test, helper$token)
+  adls_test <- adls(adls_url(helper$account_test_name), helper$token)
 
   expect_is(adls_test, "adls")
 })
